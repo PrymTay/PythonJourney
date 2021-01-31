3a11efcf6210ea64f00c2e19c84208b07e32cf72
Primrose <40164576+PrymTay@users.noreply.github.com>
2021-01-31 09:57:57 -1100

    Add files via upload
    variables and control flow

---
+++ basics.py
@@ -0,0 +1,28 @@
+#variables
+name = "Prim"
+Prim = "This is variable of type string"
+Age = 83         #this is on type interger
+Married = False   #This is of type Boolean
+weight = 48.5   # this is of type float
+
+#Control Flow
+if Age > 36:
+    print("Girl, get married!")
+elif Age <36:
+    print("You're still young...kinda") 
+else:
+    print("i'm Confused")      
+
+if Age == 23 and weight == 48.5:
+    print("I know you are working hard Prim")
+else:
+   print("i don't know you")
+
+if True:
+    print("Hello world")
+else:
+    print("Python is fun")
+
+#LOOPING
+#we can use a while loop or a for loop t loop in python.
+
